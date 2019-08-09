4fe278c31bbfcd3821547cf82a5a388ef694d71e
Aaron Miller <milleaar@oregonstate.edu>
2019-08-08 20:29:13 -0500

    Added testRandomUrl method

--- FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -22,6 +22,7 @@ import java.io.*;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Random;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -71,6 +72,33 @@ protected void setUp() {
             e.printStackTrace();
       }
       assertTrue(true);
+   }
+   
+   public void testRandomUrl() {
+	   //random test
+	   for(int i = 0; i < 20000; i++) {
+		   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+
+		   StringBuilder testBuffer = new StringBuilder();
+	       boolean expected = true;
+	       
+	       Random rand = new Random();
+       
+	       for (int j = 0; j < testPartsIndex.length; ++j) {
+	          
+	          ResultPair[] part = (ResultPair[]) testUrlPartsOptions[j];
+	          
+	          //chooses a random index for the test part
+	          int index = rand.nextInt(part.length);
+	          testBuffer.append(part[index].item);
+	          expected &= part[index].valid;
+	       }
+	       String url = testBuffer.toString();
+	
+	       boolean result = urlVal.isValid(url);
+	       assertEquals(url, expected, result);
+	       
+       }
    }
 
    public void testIsValid() {
